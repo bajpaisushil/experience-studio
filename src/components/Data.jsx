0ba3ddc54141db2c2b051e5a7910b7394a2d9b73
sushil2003 <sushilbajpai2003@gmail.com>
2023-08-02 10:18:15 +0530

    graphicons data

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -33,105 +33,105 @@ function Data() {
           <div onClick={handleClick} className="graph-icon graph-icon-1">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-1.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-2">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-2.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-3">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-3.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-4">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-4.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-5">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-5.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-6">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-6.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-7">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-7.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-8">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-8.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-9">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-9.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-10">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-10.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-11">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-11.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-12">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-12.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-13">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-13.png`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-14">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-14.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
           <div onClick={handleClick} className="graph-icon graph-icon-15">
             <img
               className={`graph-icon-image`}
-              src={require(`../images/chatgpt-icon.png`)}
+              src={require(`../images/exp-icon-15.jpg`)}
               alt='chatgpt-icon'
             />
           </div>
